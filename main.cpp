2b26f21ffe15cf6a428cff9ad835b71e56ce5098
Denis Linnik <lindeni@mail.ru>
2014-10-21 14:55:34 +0400

    timing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <ctime>
+
 
 
 int main()
 {
-    std::cout << "Hello World!" << endl;
+
+    std::clock_t    start;
+
+    start = std::clock();
+         // your test
+
+
+    int n = 1000000000;
+    while(n != 0) {
+        //std::cout << "Hello World!" << std::endl;
+        --n;
+    }
+
+
+    std::cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+    //std::cout << std::endl << stop - start << std::endl;
+
     return 0;
 }
 
