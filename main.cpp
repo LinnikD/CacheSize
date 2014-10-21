31728e82023a3aee229652b5083607802d5f3d83
Denis Linnik <lindeni@mail.ru>
2014-10-21 15:19:17 +0400

    time calc

--- main.cpp
+++ main.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
 
-
+double calcTime(std::clock_t start, std::clock_t stop) {
+    return (stop - start) / (double)(CLOCKS_PER_SEC / 1000);
+}
 
 int main()
 {
 
-    std::clock_t    start;
+    std::clock_t start;
 
     start = std::clock();
-         // your test
 
 
     int n = 1000000000;
     while(n != 0) {
-        //std::cout << "Hello World!" << std::endl;
         --n;
     }
 
-
-    std::cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
-    //std::cout << std::endl << stop - start << std::endl;
 
     return 0;
 }
