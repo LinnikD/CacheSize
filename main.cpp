d342da2a221a3ae5b736f1f8ea0cfca006aebc0d
Denis Linnik <lindeni@mail.ru>
2014-10-22 14:54:03 +0400

    compare linear and random access

--- main.cpp
+++ main.cpp
@@ -1,24 +1,104 @@
 #include <iostream>
 #include <ctime>
 #include <vector>
+#include <stdlib.h>
+
+/*
+class Node {
+public:
+    Node * next_sequential;
+    Node * next_random;
+    int * val;
+};
+
+class List {
+public:
+    List(Node * n, int size) {
+
+    }
+    getNextSeq() {
+
+    }
+    getNextRend() {
+
+    }
+    void addNode(node) {
+
+    }
+
+
+private:
+    Node * head;
+    Node * current;
+
+};*/
 
 double calcTime(std::clock_t start, std::clock_t stop) {
     return (stop - start) / (double)(CLOCKS_PER_SEC / 1000);
 }
 
+int * mix(int *a, int size) {
+    int first;
+    int second;
+    for(int i = 0; i < size * 4; i++) {
+        first = rand() % size;
+        second = rand() % size;
+        std::swap(a[first], a[second]);
+    }
+
+    return a;
+}
+
+
+void cacheTest(size_t size) {
+    int * a = new int[size];
+    for(unsigned i = 0; i < size; i ++) {
+        a[i] = i;
+    }
+
+    std::vector<int> *data = new std::vector<int>;
+    for(unsigned i = 0; i < size; i++) {
+        data->push_back(i);
+    }
+
+    long long sum = 0;
+
+    std::clock_t start = clock();
+    for(unsigned i = 0; i < size; i++) {
+        sum += data->at(a[i]);
+    }
+    std::clock_t stop = clock();
+
+    std::cout << sum << std::endl;
+    std::cout << calcTime(start, stop) << " ms" << std::endl;
+
+    mix(a,size);
+    sum = 0;
+
+    start = clock();
+    for(unsigned i = 0; i < size; i++) {
+        sum += data->at(a[i]);
+    }
+    stop = clock();
+
+    std::cout << sum <<std::endl;
+    std::cout << calcTime(start, stop) << " ms" << std::endl;
+
+    delete[] a;
+    delete data;
+}
+
+
+
 int main()
 {
 
-    std::clock_t start;
+    const size_t kb = 1024;
+    const size_t mb = 1024 * kb;
 
-    start = std::clock();
+    const size_t size =  1 * mb / sizeof(int);
 
-
-    int n = 1000000000;
-    while(n != 0) {
-        --n;
-    }
-
+    cacheTest(size);
 
     return 0;
 }
